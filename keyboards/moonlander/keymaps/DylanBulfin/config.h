ad7954fbf38fb69865ea6f016396f805f8e4fd79
Dylan Bulfin <dylanbulfin@gmail.com>
2024-05-11 14:57:42 -0400

    Adding common config

--- keyboards/moonlander/keymaps/DylanBulfin/config.h
+++ keyboards/moonlander/keymaps/DylanBulfin/config.h
@@ -1,19 +1,9 @@
+#include "common.h"
 /*
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-
-#define PERMISSIVE_HOLD_PER_KEY
-
-#define TAPPING_TERM_PER_KEY
-
-#define QUICK_TAP_TERM_PER_KEY
-
-#define ACHORDION_STREAK
-
-#define DUMMY_MOD_NEUTRALIZER_KEYCODE KC_RIGHT_CTRL
-#define MODS_TO_NEUTRALIZE { MOD_LGUI, MOD_LALT }
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define FIRMWARE_VERSION u8"EoZp5/x4anW"
