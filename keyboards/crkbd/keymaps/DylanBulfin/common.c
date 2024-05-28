9f0079224c1c7ec9d5dad1b8276c8f7356cb56b5
Dylan Bulfin <dylanbulfin@gmail.com>
2024-05-28 13:19:22 -0400

    Adding rgb to corne

--- keyboards/crkbd/keymaps/DylanBulfin/common.c
+++ keyboards/crkbd/keymaps/DylanBulfin/common.c
@@ -61,3 +61,138 @@ void matrix_scan_user(void) {
 /**/
 /*  return true;*/
 /*}*/
+
+// OLED Display
+
+#ifdef OLED_ENABLE
+
+void fill_screen(char *str) {
+    for (int i = 0; i < 5; i++) {
+        oled_set_cursor(0, i);
+        oled_write(str, false);
+    }
+}
+
+bool oled_task_master(void) {
+    // Host Keyboard Layer Status
+    /*oled_write_P(PSTR("Layer: "), false);*/
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            fill_screen("Base\n");
+            break;
+        case 1:
+            fill_screen("Navigation\n");
+            break;
+        case 2:
+            fill_screen("Numbers\n");
+            break;
+        case 3:
+            fill_screen("Symbols\n");
+            break;
+        case 4:
+            fill_screen("Function\n");
+            break;
+        case 5:
+            fill_screen("Mods\n");
+            break;
+        case 6:
+            fill_screen("QWERTY\n");
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    return false;
+}
+
+bool oled_task_indentured_servant(void) {
+    oled_write(get_u8_str(get_current_wpm(), '0'), false);
+    return false;
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        return oled_task_master();
+    } else {
+        return oled_task_indentured_servant();
+    }
+
+    return false;
+}
+
+void oled_render_boot(bool bootloader) {
+    oled_clear();
+    for (int i = 0; i < 4; i++) {
+        oled_set_cursor(0, i);
+        if (bootloader) {
+            oled_write_P(PSTR("Awaiting New Firmware "), false);
+        } else {
+            oled_write_P(PSTR("Rebooting "), false);
+        }
+    }
+
+    oled_render_dirty(true);
+}
+
+bool shutdown_user(bool jump_to_bootloader) {
+    oled_render_boot(jump_to_bootloader);
+
+    return true;
+}
+
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+extern rgb_config_t rgb_matrix_config;
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_enable();
+}
+
+extern const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3];
+void                         set_layer_color(int layer) {
+    for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+        HSV hsv = {
+                                    .h = pgm_read_byte(&ledmap[layer][i][0]),
+                                    .s = pgm_read_byte(&ledmap[layer][i][1]),
+                                    .v = pgm_read_byte(&ledmap[layer][i][2]),
+        };
+        if (!hsv.h && !hsv.s && !hsv.v) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        } else {
+            RGB   rgb = hsv_to_rgb(hsv);
+            float f   = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+            rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
+        }
+    }
+}
+
+bool rgb_matrix_indicators_user(void) {
+    switch (biton32(layer_state)) {
+        case 0:
+            set_layer_color(0);
+            break;
+        case 1:
+            set_layer_color(1);
+            break;
+        case 2:
+            set_layer_color(2);
+            break;
+        case 3:
+            set_layer_color(3);
+            break;
+        case 4:
+            set_layer_color(4);
+            break;
+        case 5:
+            set_layer_color(5);
+            break;
+        default:
+            if (rgb_matrix_get_flags() == LED_FLAG_NONE) rgb_matrix_set_color_all(0, 0, 0);
+            break;
+    }
+    return true;
+}
+#endif
--- keyboards/crkbd/keymaps/DylanBulfin/keymap.c
+++ keyboards/crkbd/keymaps/DylanBulfin/keymap.c
@@ -19,93 +19,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "features/achordion.h"
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_ESC, KC_LCTL, LALT_T(KC_A), LGUI_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G, KC_M, LSFT_T(KC_N), LCTL_T(KC_E), LGUI_T(KC_I), LALT_T(KC_O), KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, OSM(MOD_LSFT), LT(2, KC_BSPC), LT(3, KC_ENT), LT(6, KC_ESC), LT(1, KC_SPC), LT(4, KC_TAB)),
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_ESC, KC_LCTL, LALT_T(KC_A), LGUI_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G, KC_M, LSFT_T(KC_N), LCTL_T(KC_E), LGUI_T(KC_I), LALT_T(KC_O), KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(5), LT(2, KC_BSPC), LT(3, KC_ENT), LSFT_T(KC_ESC), LT(1, KC_SPC), LT(4, KC_TAB)),
                                                               [1] = LAYOUT_split_3x6_3(KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_END), LCTL(KC_HOME), KC_NO, KC_NO, QK_BOOT, KC_NO, KC_BSPC, KC_ENT, KC_NO, KC_NO, KC_NO),
-                                                              [2] = LAYOUT_split_3x6_3(KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_SPC, KC_0),
-                                                              [3] = LAYOUT_split_3x6_3(KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, KC_NO, KC_COLN, KC_CIRC, KC_PERC, KC_DLR, KC_PLUS, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_SPC, KC_RPRN),
-                                                              [4] = LAYOUT_split_3x6_3(KC_TAB, KC_F12, KC_F7, KC_F8, KC_F9, KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, KC_F11, KC_F4, KC_F5, KC_F6, KC_CAPS, KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_LSFT, KC_F10, KC_F1, KC_F2, KC_F3, QK_REP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(5), KC_BSPC, KC_ENT, KC_NO, KC_NO, KC_NO),
-                                                              [5] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_NO, KC_NO, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+                                                              [2] = LAYOUT_split_3x6_3(KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, KC_NO, KC_QUOT, KC_4, KC_5, KC_6, KC_EQL, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_0, KC_NO),
+                                                              [3] = LAYOUT_split_3x6_3(KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, KC_NO, KC_DQUO, KC_CIRC, KC_PERC, KC_DLR, KC_PLUS, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_RPRN, KC_NO),
+                                                              [4] = LAYOUT_split_3x6_3(KC_TAB, KC_F12, KC_F7, KC_F8, KC_F9, KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, KC_F11, KC_F4, KC_F5, KC_F6, KC_CAPS, KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_LSFT, KC_F10, KC_F1, KC_F2, KC_F3, QK_REP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(6), KC_BSPC, KC_ENT, KC_NO, KC_NO, KC_NO),
+                                                              [5] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_NO, KC_NO, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
                                                               [6] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_TRNS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC, KC_TRNS, KC_ENT, KC_BSPC, KC_SPC, TO(0), KC_NO)};
-
-#ifdef OLED_ENABLE
-
-void fill_screen(char *str) {
-    for (int i = 0; i < 5; i++) {
-        oled_set_cursor(0, i);
-        oled_write(str, false);
-    }
-}
-
-bool oled_task_master(void) {
-    // Host Keyboard Layer Status
-    /*oled_write_P(PSTR("Layer: "), false);*/
-
-    switch (get_highest_layer(layer_state)) {
-        case 0:
-            fill_screen("Base\n");
-            break;
-        case 1:
-            fill_screen("Navigation\n");
-            break;
-        case 2:
-            fill_screen("Numbers\n");
-            break;
-        case 3:
-            fill_screen("Symbols\n");
-            break;
-        case 4:
-            fill_screen("Function\n");
-            break;
-        case 5:
-            fill_screen("Mods\n");
-            break;
-        case 6:
-            fill_screen("QWERTY\n");
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-
-    return false;
-}
-
-bool oled_task_indentured_servant(void) {
-    oled_write(get_u8_str(get_current_wpm(), '0'), false);
-    return false;
-}
-
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        return oled_task_master();
-    } else {
-        return oled_task_indentured_servant();
-    }
-
-    return false;
-}
-
-void oled_render_boot(bool bootloader) {
-    oled_clear();
-    for (int i = 0; i < 4; i++) {
-        oled_set_cursor(0, i);
-        if (bootloader) {
-            oled_write_P(PSTR("Awaiting New Firmware "), false);
-        } else {
-            oled_write_P(PSTR("Rebooting "), false);
-        }
-    }
-
-    oled_render_dirty(true);
-}
-
-bool shutdown_user(bool jump_to_bootloader) {
-    oled_render_boot(jump_to_bootloader);
-
-    return true;
-}
-
-#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_achordion(keycode, record)) {
@@ -114,3 +34,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     return true;
 }
+
+const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
+    [0] = {{205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211},
+
+           {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}, {205, 228, 211}},
+
+    [1] = {{0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245},
+
+           {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}},
+
+    [2] = {{74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206},
+
+           {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}, {74, 255, 206}},
+
+    [3] = {{212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255},
+
+           {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}, {212, 230, 255}},
+
+    [4] = {{139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201},
+
+           {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}, {139, 230, 201}},
+
+    [5] = {{41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255},
+
+           {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}, {41, 255, 255}},
+};
